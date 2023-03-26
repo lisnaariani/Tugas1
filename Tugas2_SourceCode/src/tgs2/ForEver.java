51100a3748c23e336624cc7eddbfd877018fa06c
LISNAARIANI_230 <127526905+lisnaariani@users.noreply.github.com>
2023-03-27 01:28:02 +0800

    Add files via upload

---
+++ Tugas2_SourceCode/src/tgs2/ForEver.java
@@ -0,0 +1,22 @@
+/* NIM      : 13020210230
+Nama        : Lisna Ariani
+Hari/Tgl    : Minggu, 26 Maret 2023
+Waktu       : 13.06  PM */
+package tgs2;
+
+public class ForEver {
+
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Program */
+        System.out.println("Program akan looping, akhiri dengan ^c");
+        while (true){
+            System.out.print ("Print satu baris ....\n");
+        }
+    
+    }
+    
+}
